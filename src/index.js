7d97ae09e9dcdad7659c29b9c9ef89a0efa6f218
Alex Boh <cmextw2610@yandex.ru>
2020-10-16 02:05:25 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length % 2)
+        return false;
+
+    for (let i = 1; i < str.length; i++) {
+        let chCurr = str[i],
+            chPrev = str[i - 1];
+
+        for (let config of bracketsConfig) {
+            if (chPrev === config[0] && chCurr === config[1])
+                return check(str.slice(0, i - 1) + str.slice(i + 1), bracketsConfig);
+        }
+    }
+
+    return !str.length;
+};
